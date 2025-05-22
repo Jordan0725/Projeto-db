05a3e5a06b0ee0d98bd13c5f424520701ec8a3d5
JORDAN SOARES AGUIAR <jordan_aguiar@estudante.sesisenai.org.br>
2025-05-22 12:36:23 -0300

    parte 1

---
+++ dbprojeto/db.js
@@ -0,0 +1,39 @@
+async function connect() {  
+    const { Pool } = require("pg");
+
+    if(global.connection)
+        return global.connection.connect();
+
+    const pool = new Pool({
+      user: process.env.USER_NAME,
+      host: process.env.HOST_NAME,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      dialect: process.env.DB_DIALECT,
+      port: process.env.PORT_NUMBER
+    });
+    
+    const client = await pool.connect();
+    console.log("O Pool de conexão foi criado com sucesso!")
+    client.release();
+
+    global.connection = pool;
+    
+    return pool.connect();
+  }
+
+  connect();
+
+  //Função para inserir clientes 
+  async function insertCustomers(customer){
+    const client = await connect();
+    //Estabelecer conexão
+    const sql = "INSERT INTO client(cpf, nome, email, idade, profissao ) VALUES($1, $2, $3, $4, $5);";
+    // query
+    const values = [customer.cpf, customer.nome, customer.email, customer.idade, customer.profissao];
+
+    await client.query(sql, values);
+  }
+  module.exports ={
+    insertCustomers
+  }
